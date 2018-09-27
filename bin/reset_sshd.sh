506303aef7d5889e33932795d45c6a817d60fa2e
LordAlfredo <daniel.chaim.anderson@gmail.com>
2018-09-27 10:50:13 -0700

    Licensing headers

--- bin/reset_sshd.sh
+++ bin/reset_sshd.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Copyright 2012-2014 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
 modified=false
 
 # Remove EC2 Instance Connect sshd config if present
@@ -18,7 +31,7 @@ fi
 
 if [ $modified = true ] ; then
     # Restart sshd
-    # HACK: There is no good way to tell what init system is running.
+    # HACK: There is absolutely no good way to tell what init system is running.
     # "Best" solution is to just try them all
     sudo systemctl restart ssh || true
     sudo service sshd restart || true
